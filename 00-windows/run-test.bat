7d53508ca84aa2730751096106346052124553a2
CF <carsten.fischer@yahoo.de>
2024-05-15 09:45:24 +0200

    More output

--- 00-windows/run-test.bat
+++ 00-windows/run-test.bat
@@ -1,11 +1,17 @@
+@echo off
+
 rem
 rem Create test decks
 rem
 
 pushd ..
 
+echo Creating test decks ...
+
 python ./src/obsankipy.py ./examples/vault/.obsankipy/config.yaml
 if ERRORLEVEL 1 ( echo ERROR & exit /b 1 )
+
+echo DONE!
 
 pause
 
